cb3829931b5ce78db2f84b00cec3c7be0e255b84
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-06-11 18:06:33 +0530

    Add files via upload

---
+++ required-things-ubuntu-efx.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+echo $red
+echo '
+ _____ _______  __  _____
+| ____|  ___\ \/ / |_   _|_   __
+|  _| | |_   \  /    | | \ \ / /
+| |___|  _|  /  \    | |  \ V /
+|_____|_|   /_/\_\   |_|   \_/--Youtube
+/....Youneed Internet (Connect to wifi)....\'
+echo " "
+echo "### This Script is written by EFX Tv "
+echo " "
+echo "---------We are ready to start the Installation Process ---------️----"
+sleep 4
+echo " ---------1"
+sleep 1
+echo " -----------2"
+sleep 1
+echo " ------------3"
+sleep 1
+echo " -------------4"
+sleep 1
+echo " --------------5"
+sleep 1
+echo " ---------------6"
+sleep 1
+echo " ----------------7"
+sleep 1
+echo " -----------------8"
+sleep 1
+echo " ------------------9"
+sleep 1
+echo " -------------------10"
+sleep 1
+echo " -------------------Reasy to Install"
+sleep 3
+echo " -------------------------------Every thing is okay"
+sleep 1
+echo " -------------------Getting Started"
+sleep 1
+echo " -------------------Reasy to Install"
+sleep 1
+echo " -------------------Installing..."
+sleep 2
+echo " "
+echo " Installing update and upgrade..."
+sleep 2
+sudo apt-get update -y
+sudo apt-get upgrade -y
+clear
+echo " "
+echo " "
+echo " Installing Ruby..."
+sleep 4
+sudo apt install ruby -y
+clear
+echo " "
+echo " "
+echo " Installing wget..."
+sleep 4
+sudo apt install wget -y
+clear
+echo " "
+echo " "
+echo " Installing curl..."
+sleep 4
+sudo apt install curl -y
+clear
+echo " "
+echo " "
+echo " Installing git..."
+sleep 4
+sudo apt install git -y
+clear
+echo " "
+echo " "
+echo " Installing Gedit Text Editor..."
+sleep 4
+sudo apt install gedit -y
+clear
+echo " "
+echo " "
+echo " Installing Apache2..."
+sleep 4
+sudo apt install apache2 -y
+clear
+echo " "
+echo " "
+echo " Installing Postgresql..."
+sleep 4
+sudo apt install postgresql -y
+clear
+echo " "
+echo " "
+echo " Installing update and upgrade..."
+sleep 4
+sudo apt-get update -y && sudo apt-get upgrade -y
+clear
+echo " "
+echo " "
+echo " Installing Snap..."
+sleep 4
+sudo apt install snap -y
+clear
+echo " "
+echo " "
+echo " Installing Python..."
+sleep 4
+sudo apt install python -y
+clear
+echo " "
+echo " "
+echo " Installing Python2..."
+sleep 4
+sudo apt install python2 -y
+clear
+echo " "
+echo " "
+echo " Installing Python3..."
+sleep 4
+sudo apt install python3 -y
+clear
+echo " "
+echo " "
+echo " Installing Php..."
+sleep 4
+sudo apt install php -y
+clear
+echo " "
+echo " "
+echo " Installing Open-JDK-8-Jre..."
+sleep 4
+sudo apt install openjdk-8-jdk openjdk-8-jre -y
+clear
+echo " "
+echo " "
+echo " Installing ftp..."
+sleep 4
+sudo apt install ftp -y
+clear
+echo " "
+echo " "
+echo " Installing python3-pip..."
+sleep 4
+sudo apt install python3-pip -y
+clear
+echo " "
+echo " "
+echo " Installing updates..."
+sleep 4
+sudo apt-get update -y
+clear
+echo " "
+echo " "
+echo " Installing python-pip..."
+sleep 4
+sudo apt install python-pip -y
+clear
+echo " "
+echo " "
+echo " Installing locate..."
+sleep 4
+sudo apt install locate -y
+clear
+echo " "
+echo " "
+echo " Installing Tree..."
+sleep 4
+sudo apt install tree -y
+clear
+echo " "
+echo " "
+echo " Installing Some more Required things..."
+sleep 4
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 -y
+
+
+echo " "
+echo "...############# Cheers !!! ################..."
+clear
+echo " ."
+sleep 1
+echo " ."
+sleep 1
+echo " ."
+sleep 1
+echo " ."
+sleep 1
+echo " ."
+echo " All supported files are installed, You are ready to use the linux"
+sleep 2
+echo '
+/.... (PLEASE)
+ ____  _   _ ____ ____   ____ ____  ___ ____  _____
+/ ___|| | | | __ ) ___| / ___|  _ \|_ _| __ )| ____|
+\___ \| | | |  _ \___ \| |   | |_) || ||  _ \|  _|
+ ___) | |_| | |_) |__) | |___|  _ < | || |_) | |___
+|____/ \___/|____/____/ \____|_| \_\___|____/|_____|
+
+------------------------------------(THANK YOU)....\'
+sleep 4
+exit
